57b33bbe7bd12de93157175b9ba9f71f0a5af6cf
Vincent Parrett <vincent@finalbuilder.com>
2021-03-01 12:10:38 +1100

    Fixed batch file to follow redirects.

--- Build/DownloadPackages.bat
+++ Build/DownloadPackages.bat
@@ -1,22 +1,33 @@
-curl "https://github.com/VSoftTechnologies/Spring4DMirror/releases/download/v2.0.0-dev5/Spring4D.Base-10.4-Win32-2.0.0-dev5.dpkg" --output %1%\Spring4D.Base-10.4-Win32-2.0.0-dev5.dpkg
-curl "https://github.com/VSoftTechnologies/Spring4DMirror/releases/download/v2.0.0-dev5/Spring4D.Core-10.4-Win32-2.0.0-dev5.dpkg" --output %1%\Spring4D.Core-10.4-Win32-2.0.0-dev5.dpkg
-curl "https://github.com/VSoftTechnologies/Spring4DMirror/releases/download/v2.0.0-dev5/Spring4D.Extensions-10.4-Win32-2.0.0-dev5.dpkg" --output %1%\Spring4D.Extensions-10.4-Win32-2.0.0-dev5.dpkg
+IF "%~1" == "" GOTO need_param
+goto do_download
 
-curl "https://github.com/VSoftTechnologies/VSoft.AntPatterns/releases/download/v0.1.0/VSoft.AntPatterns-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.AntPatterns-10.4-Win32-0.1.0.dpkg
+:need_param
+echo "usage: DownloadPackages c:\dpmfeed"
+exit /b
 
-curl "https://github.com/VSoftTechnologies/JsonDataObjects/releases/download/v0.1.0/VSoft.JsonDataObjects-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.JsonDataObjects-10.4-Win32-0.1.0.dpkg
+:do_download
 
-curl "https://github.com/VSoftTechnologies/VSoft.Uri/releases/download/v0.1.0/VSoft.Uri-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.Uri-10.4-Win32-0.1.0.dpkg
+curl -L "https://github.com/VSoftTechnologies/Spring4DMirror/releases/download/v2.0.0-dev5/Spring4D.Base-10.4-Win32-2.0.0-dev5.dpkg" --output %1%\Spring4D.Base-10.4-Win32-2.0.0-dev5.dpkg
+curl -L "https://github.com/VSoftTechnologies/Spring4DMirror/releases/download/v2.0.0-dev5/Spring4D.Core-10.4-Win32-2.0.0-dev5.dpkg" --output %1%\Spring4D.Core-10.4-Win32-2.0.0-dev5.dpkg
+curl -L "https://github.com/VSoftTechnologies/Spring4DMirror/releases/download/v2.0.0-dev5/Spring4D.Extensions-10.4-Win32-2.0.0-dev5.dpkg" --output %1%\Spring4D.Extensions-10.4-Win32-2.0.0-dev5.dpkg
 
-curl "https://github.com/VSoftTechnologies/VSoft.SemanticVersion/releases/download/v0.2.0/VSoft.SemanticVersion-10.4-Win32-0.2.0.dpkg" --output %1%\VSoft.SemanticVersion-10.4-Win32-0.2.0.dpkg
+curl -L "https://github.com/VSoftTechnologies/VSoft.AntPatterns/releases/download/v0.1.0/VSoft.AntPatterns-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.AntPatterns-10.4-Win32-0.1.0.dpkg
 
-curl "https://github.com/VSoftTechnologies/VSoft.HttpClient/releases/download/v0.1.0/VSoft.HttpClient-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.HttpClient-10.4-Win32-0.1.0.dpkg
+curl -L "https://github.com/VSoftTechnologies/JsonDataObjects/releases/download/v0.1.0/VSoft.JsonDataObjects-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.JsonDataObjects-10.4-Win32-0.1.0.dpkg
 
-curl "https://github.com/VSoftTechnologies/VSoft.CommandLineParser/releases/download/v0.1.0/VSoft.CommandLine-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.CommandLine-10.4-Win32-0.1.0.dpkg
+curl -L "https://github.com/VSoftTechnologies/VSoft.Uri/releases/download/v0.1.0/VSoft.Uri-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.Uri-10.4-Win32-0.1.0.dpkg
 
-curl "https://github.com/VSoftTechnologies/VSoft.VirtualListView/releases/download/v0.3.0-rc6/VSoft.VirtualListView-10.4-Win32-0.3.0-rc6.dpkg" --output %1%\VSoft.VirtualListView-10.4-Win32-0.3.0-rc6.dpkg
+curl -L "https://github.com/VSoftTechnologies/VSoft.SemanticVersion/releases/download/v0.2.0/VSoft.SemanticVersion-10.4-Win32-0.2.0.dpkg" --output %1%\VSoft.SemanticVersion-10.4-Win32-0.2.0.dpkg
 
-curl "https://github.com/VSoftTechnologies/VSoft.CancellationToken/releases/download/v0.0.2/VSoft.CancellationToken-10.4-Win32-0.0.2.dpkg" --output %1%\VSoft.CancellationToken-10.4-Win32-0.0.2.dpkg
+curl -L "https://github.com/VSoftTechnologies/VSoft.HttpClient/releases/download/v0.1.0/VSoft.HttpClient-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.HttpClient-10.4-Win32-0.1.0.dpkg
 
-curl "https://github.com/VSoftTechnologies/VSoft.Awaitable/releases/download/v0.2.0/VSoft.Awaitable-10.4-Win32-0.2.0.dpkg" -output %1%\VSoft.Awaitable-10.4-Win32-0.2.0.dpkg
+curl -L "https://github.com/VSoftTechnologies/VSoft.CommandLineParser/releases/download/v0.1.0/VSoft.CommandLine-10.4-Win32-0.1.0.dpkg" --output %1%\VSoft.CommandLine-10.4-Win32-0.1.0.dpkg
+
+curl -L "https://github.com/VSoftTechnologies/VSoft.VirtualListView/releases/download/v0.3.0-rc6/VSoft.VirtualListView-10.4-Win32-0.3.0-rc6.dpkg" --output %1%\VSoft.VirtualListView-10.4-Win32-0.3.0-rc6.dpkg
+
+curl -L "https://github.com/VSoftTechnologies/VSoft.CancellationToken/releases/download/v0.0.2/VSoft.CancellationToken-10.4-Win32-0.0.2.dpkg" --output %1%\VSoft.CancellationToken-10.4-Win32-0.0.2.dpkg
+
+curl -L "https://github.com/VSoftTechnologies/VSoft.Awaitable/releases/download/v0.2.0/VSoft.Awaitable-10.4-Win32-0.2.0.dpkg" -output %1%\VSoft.Awaitable-10.4-Win32-0.2.0.dpkg
+
+curl -L "https://github.com/VSoftTechnologies/DUnitX/releases/download/v0.1.3/VSoft.DUnitX-10.4-Win32-0.1.3.dpkg" --output %1%\VSoft.DUnitX-10.4-Win32-0.1.3.dpkg
 
