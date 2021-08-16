64fffaa7e4d6d03b04c2d02f9af3a03094ada91f
vincentparrett <vincent@finalbuilder.com>
2021-08-16 18:36:29 +1000

    Added info command to show config file in use.

---
+++ Source/Cmdline/Commands/DPM.Console.Command.Info.pas
@@ -0,0 +1,76 @@
+{***************************************************************************}
+{                                                                           }
+{           Delphi Package Manager - DPM                                    }
+{                                                                           }
+{           Copyright © 2019 Vincent Parrett and contributors               }
+{                                                                           }
+{           vincent@finalbuilder.com                                        }
+{           https://www.finalbuilder.com                                    }
+{                                                                           }
+{                                                                           }
+{***************************************************************************}
+{                                                                           }
+{  Licensed under the Apache License, Version 2.0 (the "License");          }
+{  you may not use this file except in compliance with the License.         }
+{  You may obtain a copy of the License at                                  }
+{                                                                           }
+{      http://www.apache.org/licenses/LICENSE-2.0                           }
+{                                                                           }
+{  Unless required by applicable law or agreed to in writing, software      }
+{  distributed under the License is distributed on an "AS IS" BASIS,        }
+{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. }
+{  See the License for the specific language governing permissions and      }
+{  limitations under the License.                                           }
+{                                                                           }
+{***************************************************************************}
+
+unit DPM.Console.Command.Info;
+
+interface
+uses
+  VSoft.Awaitable,
+  DPM.Console.Writer,
+  DPM.Console.ExitCodes,
+  DPM.Console.Command,
+  DPM.Console.Command.Base,
+  DPM.Core.Configuration.Interfaces,
+  DPM.Core.Logging;
+
+type
+  TInfoCommand = class(TBaseCommand)
+  private
+    FConsole : IConsoleWriter;
+
+  protected
+    function Execute(const cancellationToken : ICancellationToken) : TExitCode; override;
+  public
+    constructor Create(const console : IConsoleWriter; const logger : ILogger; const configurationManager : IConfigurationManager);reintroduce;
+  end;
+
+
+implementation
+
+uses
+  DPM.Console.Banner,
+  DPM.Core.Options.Common,
+  DPM.Core.Options.Info;
+
+
+constructor TInfoCommand.Create(const console: IConsoleWriter; const logger: ILogger; const configurationManager: IConfigurationManager);
+begin
+  inherited Create(logger, configurationManager);
+  FConsole := console;
+
+end;
+
+function TInfoCommand.Execute(const cancellationToken : ICancellationToken) : TExitCode;
+begin
+  ShowBanner(FConsole);
+
+  TInfoOptions.Default.ApplyCommon(TCommonOptions.Default);
+
+
+  Logger.Information('DPM Config file : '  + TInfoOptions.Default.ConfigFile);
+  result := TExitCode.OK;
+end;
+end.
--- Source/dpm.dpr
+++ Source/dpm.dpr
@@ -179,7 +179,9 @@ uses
   DPM.Core.Package.Icon in 'Core\Package\DPM.Core.Package.Icon.pas',
   DPM.Core.Compiler.BOM in 'Core\Compiler\DPM.Core.Compiler.BOM.pas',
   DPM.Core.Compiler.ProjectSettings in 'Core\Compiler\DPM.Core.Compiler.ProjectSettings.pas',
-  DPM.Core.Utils.Files in 'Core\Utils\DPM.Core.Utils.Files.pas';
+  DPM.Core.Utils.Files in 'Core\Utils\DPM.Core.Utils.Files.pas',
+  DPM.Console.Command.Info in 'Cmdline\Commands\DPM.Console.Command.Info.pas',
+  DPM.Core.Options.Info in 'Core\Options\DPM.Core.Options.Info.pas';
 
 begin
   CoInitializeEx(nil, COINIT_MULTITHREADED); //needed for msxml
