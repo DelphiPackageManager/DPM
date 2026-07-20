fc644eb8d390bf4cd68a53666c5732a83f50fb4d
Vincent Parrett <vincent@finalbuilder.com>
2026-07-20 16:42:30 +1000

    package updates

---
+++ Source/uninstallpkg.bat
@@ -0,0 +1,18 @@
+dpm uninstall %1 dpm.dproj
+dpm uninstall %1 dpm.core.tests.dproj -compiler=13
+dpm uninstall %1 DSpecCreator\DspecCreator.dproj -compiler=13
+dpm uninstall %1 dpm.ide.d130.dproj -compiler=13
+dpm uninstall %1 dpm.ide.d120.dproj -compiler=12
+dpm uninstall %1 dpm.ide.d110.dproj -compiler=11
+dpm uninstall %1 dpm.ide.d104.dproj -compiler=10.4
+dpm uninstall %1 dpm.ide.d103.dproj -compiler=10.3
+dpm uninstall %1 dpm.ide.d102.dproj -compiler=10.2
+dpm uninstall %1 dpm.ide.d101.dproj -compiler=10.1
+dpm uninstall %1 dpm.ide.d100.dproj -compiler=10.0
+dpm uninstall %1 dpm.ide.xe8.dproj -compiler=XE8
+dpm uninstall %1 dpm.ide.xe7.dproj -compiler=XE7
+dpm uninstall %1 dpm.ide.xe6.dproj -compiler=XE6
+dpm uninstall %1 dpm.ide.xe5.dproj -compiler=XE5
+dpm uninstall %1 dpm.ide.xe4.dproj -compiler=XE4
+dpm uninstall %1 dpm.ide.xe3.dproj -compiler=XE3
+dpm uninstall %1 dpm.ide.xe2.dproj -compiler=XE2
